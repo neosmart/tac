4228445e1ced52510e6c7dd2fc0b2309c96ba537
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-08-15 16:21:41 -0500

    stdin goes to memory buffer unless it's huge

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate uuid;
 use memmap::Mmap;
 use std::vec::Vec;
 
+const MAX_BUF_SIZE: usize = 4 * 1024 * 1024; //4MiB
+
 fn version() {
     println!("tac 0.1 - Copyright NeoSmart Technologies 2017");
     println!("Report bugs at <https://github.com/neosmart/tac>");
@@ -47,14 +49,15 @@ fn main() {
 
     for file in files {
         match reverse_file(file) {
-            Err(e) => eprintln!("{}", e),
+            Err(e) => panic!("{}", e),
             _ => {}
         }
     }
 }
 
 fn to_str(err: std::io::Error) -> String {
-    return format!("{}", err);
+    panic!("{}", err);
+    // return format!("{}", err);
 }
 
 fn print_bytes(bytes: &[u8]) {
@@ -65,43 +68,69 @@ fn print_bytes(bytes: &[u8]) {
 fn reverse_file(path: &str) -> Result<(), String> {
     use std::fs::File;
     use std::io::Write;
-    use std::path::PathBuf;
     use uuid::Uuid;
 
-    let mut temp_path = PathBuf::new();
+    let mmap;
+    let mut line;
+    let mut temp_path = unsafe { std::mem::uninitialized() };
+    let mut delete_on_exit = false;
 
     {
-        let path = match path {
+        let (file, len) = match path {
             "-" => {
                 //read from stdin
-                //we have no idea how big this can get, don't buffer in memory
-                temp_path = std::env::temp_dir().join(format!("{}", Uuid::new_v4().hyphenated()));
-                let mut temp_file = File::create(&temp_path).map_err(to_str)?;
-                let stdin = std::io::stdin();
+                //buffer up to MAX_BUFF_SIZE in memory
 
-                let mut line = String::new();
-                while stdin.read_line(&mut line).map_err(to_str)? != 0 {
-                    temp_file.write_all(line.as_bytes()).map_err(to_str)?;
-                    line.clear();
+                let mut in_mem = true;
+                line = String::new();
+
+                {
+                    let stdin = std::io::stdin();
+
+                    let mut temp_file = unsafe { std::mem::uninitialized() };
+                    while stdin.read_line(&mut line).map_err(to_str)? != 0 {
+                        if in_mem && line.len() > MAX_BUF_SIZE {
+                            temp_path = std::env::temp_dir()
+                                .join(format!("{}", Uuid::new_v4().hyphenated()));
+                            temp_file = File::create(&temp_path).map_err(to_str)?;
+
+                            //write everything we've read so far
+                            temp_file.write_all(line.as_bytes()).map_err(to_str)?;
+                            //assign new string and not clear because we'll be using a smaller
+                            //buffer now
+                            line = String::new();
+                            eprintln!("Switching to file mode");
+                            in_mem = false;
+                            delete_on_exit = true;
+                        } else if !in_mem {
+                            temp_file.write_all(line.as_bytes()).map_err(to_str)?;
+                            line.clear();
+                        }
+                    }
                 }
 
-                temp_path.to_str().unwrap()
-            },
-            _ => path
-        };
-
-        let file = Mmap::open_path(path, memmap::Protection::Read).map_err(to_str)?;
-        let len = file.len();
-
-        let mmap = unsafe {
-            file.as_slice()
+                match in_mem {
+                    true => (line.as_bytes(), line.as_bytes().len()),
+                    false => {
+                        mmap =
+                            Mmap::open_path(&temp_path, memmap::Protection::Read).map_err(to_str)?;
+                        let bytes = unsafe { mmap.as_slice() };
+                        (bytes, mmap.len())
+                    }
+                }
+            }
+            _ => {
+                mmap = Mmap::open_path(path, memmap::Protection::Read).map_err(to_str)?;
+                let bytes = unsafe { mmap.as_slice() };
+                (bytes, mmap.len())
+            }
         };
 
         let mut last_printed: i64 = len as i64;
         let mut index = last_printed - 1;
         while index > -2 {
-            if index == -1 || mmap[index as usize] == '\n' as u8 {
-                print_bytes(&mmap[(index + 1) as usize..last_printed as usize]);
+            if index == -1 || file[index as usize] == '\n' as u8 {
+                print_bytes(&file[(index + 1) as usize..last_printed as usize]);
                 last_printed = index + 1;
             }
 
@@ -109,9 +138,12 @@ fn reverse_file(path: &str) -> Result<(), String> {
         }
     }
 
-    if temp_path != PathBuf::new() {
-        std::fs::remove_file(&temp_path)
-            .map_err(|e| format!("Failed to remove temporary file {}: {}", temp_path.display(), e))?;
+    if delete_on_exit {
+        std::fs::remove_file(&temp_path).map_err(|e| {
+                format!("Failed to remove temporary file {}: {}",
+                        temp_path.display(),
+                        e)
+            })?;
     }
 
     return Ok(());
