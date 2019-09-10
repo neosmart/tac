e7d55de5a633cdbf4f046df4b537a9eba617ebf7
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2019-09-10 03:44:43 -0500

    Update argument parsing and update formatting

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use memmap::Mmap;
 use std::io::{Stdout, Write};
 use std::vec::Vec;
 
-const MAX_BUF_SIZE: usize = 4 * 1024 * 1024; //4MiB
+const MAX_BUF_SIZE: usize = 4 * 1024 * 1024; // 4 MiB
 
 fn version() {
-    println!("tac 0.2.3 - Copyright NeoSmart Technologies 2017");
+    println!("tac 0.2.3 - Copyright NeoSmart Technologies 2017-2019");
+    println!("Developed by Mahmoud Al-Qudsi <mqudsi@neosmart.net>");
     println!("Report bugs at <https://github.com/neosmart/tac>");
 }
 
@@ -27,28 +28,35 @@ fn help() {
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let mut files = Vec::new();
+    let mut skip_switches = false;
     for arg in args.iter().skip(1).map(|s| s.as_str()) {
-        match arg {
-            "-h" | "--help" => {
-                help();
-                std::process::exit(0);
-            }
-            "-v" | "--version" => {
-                version();
-                std::process::exit(0);
-            }
-            file => {
-                if file.starts_with("-") && file != "-" {
-                    eprintln!("Invalid option {}!", file);
+        if !skip_switches && arg.starts_with("-") && arg.len() > 1 {
+            match arg {
+                "-h" | "--help" => {
+                    help();
+                    std::process::exit(0);
+                }
+                "-v" | "--version" => {
+                    version();
+                    std::process::exit(0);
+                }
+                "--" => {
+                    skip_switches = true;
+                    continue;
+                }
+                _ => {
+                    eprintln!("{}: Invalid option!", arg);
                     eprintln!("Try 'tac --help' for more information");
                     std::process::exit(-1);
                 }
-                files.push(file)
             }
+        } else {
+            let file = arg;
+            files.push(file)
         }
     }
 
-    //read from stdin by default
+    // Read from stdin by default
     if files.len() == 0 {
         files.push("-");
     }
@@ -61,7 +69,7 @@ fn main() {
     }
 }
 
-fn print_bytes(mut stdout: &mut Stdout, bytes: &[u8]) {
+fn print_bytes(stdout: &mut Stdout, bytes: &[u8]) {
     if stdout.write_all(bytes).is_err() {
         std::process::exit(-1);
     }
@@ -69,7 +77,6 @@ fn print_bytes(mut stdout: &mut Stdout, bytes: &[u8]) {
 
 fn reverse_file(path: &str) -> std::io::Result<()> {
     use std::fs::File;
-    use std::io::Write;
     use std::path::PathBuf;
     use uuid::Uuid;
 
@@ -81,8 +88,7 @@ fn reverse_file(path: &str) -> std::io::Result<()> {
     {
         let (file, len) = match path {
             "-" => {
-                //read from stdin
-                //buffer up to MAX_BUFF_SIZE in memory
+                // Read from stdin, buffering up to MAX_BUFF_SIZE in memory
 
                 let mut in_mem = true;
                 line = String::new();
@@ -97,10 +103,10 @@ fn reverse_file(path: &str) -> std::io::Result<()> {
                                 .join(format!("{}", Uuid::new_v4().hyphenated()));
                             let mut temp_file = File::create(&temp_path)?;
 
-                            //write everything we've read so far
+                            // Write everything we've read so far
                             temp_file.write_all(line.as_bytes())?;
-                            //assign new string and not clear because we'll be using a smaller
-                            //buffer now
+                            // Assign new string and not clear because we'll be using a smaller
+                            // buffer now.
                             line = String::new();
                             in_mem = false;
                             delete_on_exit = true;
@@ -136,8 +142,10 @@ fn reverse_file(path: &str) -> std::io::Result<()> {
         let mut index = last_printed - 1;
         while index > -2 {
             if index == -1 || file[index as usize] == '\n' as u8 {
-                print_bytes(&mut stdout,
-                            &file[(index + 1) as usize..last_printed as usize]);
+                print_bytes(
+                    &mut stdout,
+                    &file[(index + 1) as usize..last_printed as usize],
+                );
                 last_printed = index + 1;
             }
 
@@ -146,9 +154,14 @@ fn reverse_file(path: &str) -> std::io::Result<()> {
     }
 
     if delete_on_exit {
-        //this should never fail unless we've somehow kept a handle open to it
-        std::fs::remove_file(&temp_path)
-            .expect(&format!("Failed to remove temporary file {}", temp_path.display()));
+        // This should never fail unless we've somehow kept a handle open to it
+        if let Err(e) = std::fs::remove_file(&temp_path) {
+            eprintln!(
+                "Error: failed to remove temporary file {}\n{}",
+                temp_path.display(),
+                e
+            )
+        };
     }
 
     return Ok(());
